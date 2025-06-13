8956375a7a547f0ebf25a5b0e826f04331f494d3
syorito-hatsuki <enderdron3@gmail.com>
2025-06-13 17:38:08 +0300

    Added Suspend component to Improve stability

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {SnackbarProvider} from "notistack";
-import {StrictMode} from "react";
+import {StrictMode, Suspense} from "react";
 import {createRoot} from "react-dom/client";
 
 import {ThemeSwitchProvider} from "@hooks/providers/ThemeSwitchProvider.tsx";
@@ -11,7 +11,9 @@ createRoot(document.getElementById("root")!).render(
         <QueryClientProvider client={new QueryClient()}>
             <ThemeSwitchProvider>
                 <SnackbarProvider>
-                    <RootPage/>
+                    <Suspense>
+                        <RootPage/>
+                    </Suspense>
                 </SnackbarProvider>
             </ThemeSwitchProvider>
         </QueryClientProvider>
